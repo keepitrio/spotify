57a38ba3caeee349938aa85c40755123b9e62ab9
Rio <rio.nose@smartsparrow.com>
2018-06-11 20:25:44 -0700

    Add function for question 3

---
+++ 3.js
@@ -0,0 +1,13 @@
+function changePossibilities(amount,denominations) {
+    const possibleCombinations = new Array(amount + 1).fill(0);
+
+    denominations.forEach((coin) => {
+        if (amount % coin === 0) possibleCombinations[coin] += 1;
+        for (let nextValue = coin; nextValue <= amount; nextValue++) {
+            const remainder = nextValue - coin;
+            possibleCombinations[nextValue] += possibleCombinations[remainder]
+        }
+    });
+
+    return possibleCombinations[amount];
+    }
